440caaf5fccf0b135dae4710a8ad25f9156abbb7
xiaopai20 <xiaopai@gmail.com>
2017-05-04 00:58:23 +0100

    fix issues

--- src/img_monitor_service.py
+++ src/img_monitor_service.py
@@ -1,35 +1,55 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-import sys, getopt, datetime
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 import cv2
+import urllib2
 
 PORT_NUMBER = 8080
 IMG_PATH = "C:\\Users\\Hannah\\Desktop\\xiaopai\\share_data\\"
+RECOGNIZE_SERVICE_URL = "http://192.168.99.100:8088/"
 
 cap = cv2.VideoCapture(0)
-curImgIndex = 1;
+curImgIndex = 0
 
 #This class will handles any incoming request from
 #the browser
-class myHandler(BaseHTTPRequestHandler):
+class myHandler(SimpleHTTPRequestHandler):
 
 	#Handler for the GET requests
 	def do_GET(self):
-	    if not self.path.endswith(".ico"):
-		    return
+		print "received " + self.path
+		if not self.path.endswith("getPersonName"):
+			return SimpleHTTPRequestHandler.do_GET(self)
+
+
 
 		self.send_response(200)
-		self.send_header('Content-type','text')
+		self.send_header('cache-control','max-age=0')
+		self.send_header('cache-control','no-cache')
+		self.send_header('Content-type','text/html')
 		self.end_headers()
 
-		ret, img = cap.read();
+		# double read to capture latest pic,
+		# other wise it will return last 2nd one, don't know why...
+		cap.read()
+		ret, img = cap.read()
 		if not ret:
-		    self.wfile.write("Capture image error")
-		    return
-		cv2.imwrite(IMG_PATH + curImgIndex + ".jpg", img)
+			self.wfile.write("Capture image error")
+			return
+
+		#cv2.imshow("input", img)
+		global curImgIndex
 		curImgIndex += 1
 		curImgIndex %= 100
 
-		self.wfile.write(IMG_PATH + curImgIndex + ".jpg")
+		imgFileName = str(curImgIndex) + ".jpg"
+		wRet = cv2.imwrite(IMG_PATH + imgFileName, img)
+		if not wRet:
+			print "saving " + imgFileName + " error"
+		print "save " + imgFileName
+
+		personName = urllib2.urlopen(RECOGNIZE_SERVICE_URL + imgFileName).read()
+		print personName
+		self.wfile.write(personName)
 
 try:
 	#Create a web server and define the handler to manage the
--- src/img_recognize_service.py
+++ src/img_recognize_service.py
@@ -2,7 +2,7 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import tensorflow as tf, sys
 
-PORT_NUMBER = 8080
+PORT_NUMBER = 8088
 IMAGE_ROOT = "/tf/share_data/"
 RETRAINED_TXT = "/tf/training/retrained_labels.txt"
 RETRAINED_GRAPH = "/tf/training/retrained_graph.pb"
@@ -23,42 +23,42 @@ softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 #the browser 
 class myHandler(BaseHTTPRequestHandler):
 
-	#Handler for the GET requests
-	def do_GET(self):
-		if self.path.endswith(".ico"):
-			return
-		self.send_response(200)
-		self.send_header('Content-type','text/html')
-		self.end_headers()
+    #Handler for the GET requests
+    def do_GET(self):
+        print "receive: " + self.path
+        if self.path.endswith(".ico"):
+            return
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
 
-                image_path = IMAGE_ROOT + self.path;
-                print(self.path)
-                image_data = tf.gfile.FastGFile(image_path, 'rb').read()
-                predictions = sess.run(softmax_tensor, \
-                    {'DecodeJpeg/contents:0': image_data})
-   
-                # Sort to show labels of first prediction in order of confidence
-                top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-    
-                for node_id in top_k:
-                   human_string = label_lines[node_id]
-                   score = predictions[0][node_id]
-                   self.wfile.write('%s (score = %.5f)' % (human_string, score))
-                if predictions[0][top_k[0]] > 0.5:
-                   self.wfile.write('=============== Hello %s =======================' % (label_lines[top_k[0]]))
+        image_path = IMAGE_ROOT + self.path
+        print(self.path)
+        image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+        predictions = sess.run(softmax_tensor, \
+            {'DecodeJpeg/contents:0': image_data})
 
-		return
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+        for node_id in top_k:
+           human_string = label_lines[node_id]
+           score = predictions[0][node_id]
+           print '%s (score = %.5f)' % (human_string, score)
+        if predictions[0][top_k[0]] > 0.5:
+           self.wfile.write(label_lines[top_k[0]])
+
 
 try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port ' , PORT_NUMBER
-	
-	#Wait forever for incoming htto requests
-	server.serve_forever()
+    #Create a web server and define the handler to manage the
+    #incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print 'Started httpserver on port ' , PORT_NUMBER
+
+    #Wait forever for incoming htto requests
+    server.serve_forever()
 
 except KeyboardInterrupt:
-	print '^C received, shutting down the web server'
-	server.socket.close()
-	
+    print '^C received, shutting down the web server'
+    server.socket.close()
+
